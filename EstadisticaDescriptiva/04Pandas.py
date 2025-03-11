e114b1a4d869ce2cfd6fef0810009517d16b0538
AE010 <armasemiliano3@gmial.com>
2025-03-10 18:29:18 -0600

    4

--- EstadisticaDescriptiva/04Pandas.py
+++ EstadisticaDescriptiva/04Pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df= pd.read_csv('./EstadisticaDescriptiva/housing.csv')
 
@@ -18,3 +19,11 @@ print('desviacion estandar de años: ', std_age)
 
 filtrodeloceano = df[df['ocean_proximity']=="ISLAND"]#filtrar
 print('Filtro de proximidad: ', filtrodeloceano)
+
+plt.scatter(df["ocean_proximity"][:10], df["median_house_value"][:10])
+
+plt.xlabel('Proximidad')
+plt.ylabel('Precio')
+
+plt.title('Grafico de Dispersion de Proximidad del oceano vs Precio')
+plt.show()
